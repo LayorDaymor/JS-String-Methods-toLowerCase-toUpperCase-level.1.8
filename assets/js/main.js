9ccc60674a5457f22b1c34e10973b932447d5581
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 18:18:43 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+var text = "Sam is going to codingschool";
+
+console.log(text.toUpperCase())
+let toUpperCase =text.toUpperCase();
+
+console.log(text.toLowerCase());
+let toLowerCase =text.toLowerCase();
+
+
+
+
+document.write("<h1>" + toUpperCase + "</h1>"+"</br>");
+document.write("<h1>" + toLowerCase + "</h1>"+"</br>");
+document.write("<h1>" + toUpperCase + "</h1>"+"</br>");
+document.write("<h1>" + toLowerCase + "</h1>"+"</br>");
+
+
+
+
+
+
+
+
+
+
+
